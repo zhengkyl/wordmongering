7744b694e74d6ecd0eb70baf0ab1ecc36ed332b5
Kyle Zheng <kylezheng73@gmail.com>
2026-05-03 03:34:43 -0400

    chore: remove apps/

--- apps/server/drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  dialect: "sqlite",
-  schema: "./schema.ts",
-  out: "./migrations",
-  dbCredentials: { url: "../../data/data.db" },
-  casing: "snake_case",
-});
